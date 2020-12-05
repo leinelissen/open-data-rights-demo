19f6b36d8bece16bb31d389065c26a4a540125d6
Lei Nelissen <lei@codified.nl>
2020-12-05 19:08:39 +0100

    Redirect to correct OAuth URL

--- api/oauth/authorize.ts
+++ api/oauth/authorize.ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@vercel/node'
 
 export default function (req: NowRequest, res: NowResponse) {
     const { redirect_uri } = req.query; 
-    res.redirect(302, `/static/?redirect=${redirect_uri}`)
+    res.redirect(302, `/oauth/callback?redirect=${redirect_uri}`)
 }
