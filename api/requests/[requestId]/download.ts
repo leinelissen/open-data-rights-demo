2ab1e3ff89c89e1d97c9b6869ae3ab268a1904e0
Lei Nelissen <lei@codified.nl>
2020-12-05 15:30:23 +0100

    Generate archive in request rather than up-front

--- api/requests/[requestId]/download.ts
+++ api/requests/[requestId]/download.ts
@@ -1,5 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node'
+import { generateArchive } from '../../../lib/generate-archive';
 
-export default function (req: NowRequest, res: NowResponse) {
-    res.redirect(302, `/public/archive.zip`);
+export default async function (req: NowRequest, res: NowResponse) {
+    const archive = await generateArchive();
+    res.send(archive);
 }
