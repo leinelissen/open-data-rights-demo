8d36514b0b959eaadc029402281d75550d4f3c76
Lei Nelissen <lei@codified.nl>
2020-12-05 17:10:26 +0100

    Add account name to /data/me

--- api/data/me.ts
+++ api/data/me.ts
@@ -1,7 +1,11 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 import withAuth from '../../lib/auth';
+import faker from 'faker';
 import { dataTypes } from '../../lib/singleRequest';
 
 export default withAuth(function (req: NowRequest, res: NowResponse) {
-    res.send(Object.keys(dataTypes));
+    res.send({
+        account: faker.internet.exampleEmail(),
+        data: Object.keys(dataTypes)
+    });
 });
