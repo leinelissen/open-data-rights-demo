a35e9181263050074302ddc6dd98ac0a0ee41e74
Lei Nelissen <lei@codified.nl>
2020-12-05 15:46:58 +0100

    Correctly set filename when downloading

--- api/requests/[requestId]/download.ts
+++ api/requests/[requestId]/download.ts
@@ -3,5 +3,7 @@ import { generateArchive } from '../../../lib/generate-archive';
 
 export default async function (req: NowRequest, res: NowResponse) {
     const archive = await generateArchive();
+    res.setHeader('Content-disposition', 'attachment; filename=archive.zip');
+    res.setHeader('Content-type', 'application/zip');
     res.send(archive);
 }
